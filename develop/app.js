7d87007fc128960514f58167def1d11f3c8b3795
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-03-04 21:31:56 -0700

    Added comment to app.js, I'm going to do a file name change for next commit

--- develop/app.js
+++ develop/app.js
@@ -1,4 +1,5 @@
-const Manager = require("./lib/Manager");
+// I have taken the liberty of importing relevant files into this file. Apologies for not commenting this section out
+const Manager = require('./lib/Manager')
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern")
 const inquirer = require("inquirer");
